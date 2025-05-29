f162f94795a6e98264f121ab802ca6d3433daea1
jyrj <jayarajevur@gmail.com>
2025-05-29 14:46:48 -0700

    proof of concept

--- gpt/Top.scala
+++
@@ -1,7 +0,0 @@
-package raptorq
-import chisel3._
-
-/** Convenience object for Verilog generation */
-object Top extends App {
-  emitVerilog(new RSEncoderHW(RaptorParams()), Array("--target-dir", "generated"))
-}
